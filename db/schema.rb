60802afbeb6697fc31f6dbd10061b49221d6d1ce
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-03-23 15:33:52 +0530

    create-user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_070210) do
+ActiveRecord::Schema.define(version: 2020_03_23_095636) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_01_27_070210) do
 
   create_table "libraries", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
