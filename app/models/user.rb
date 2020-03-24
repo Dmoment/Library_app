e8394cde9b5040eb90a44302d10d049ce9f1050d
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-03-24 19:13:53 +0530

    Users Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-
+    validates :username, presence: true, uniqueness: { case_sensitive: false },length: { in: 3..25 }
+     
+    validates :email, presence: true, uniqueness: { case_sensitive: false },length: { in: 6..150 },
+    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Not proper email"}
 end
