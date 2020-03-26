686c703e86252351883fc0eed60b8a7fdf2e9758
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-03-26 14:57:30 +0530

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :libraries
   resources :books
+  get 'signup', to:'users#new'
+  resources :users , except: [:new]
   #root "libraries#index"
   root "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
