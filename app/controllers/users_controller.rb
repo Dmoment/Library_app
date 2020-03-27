a6a80bbb1d438665c39c40cab36d5cba55051cbe
DEEPAK CHAUHAN <rishudc119@gmail.com>
2020-03-27 15:49:37 +0530

    Edit user and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,27 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:sucess]="Welcome to the Library app #{@user.username}"
-      redirect_to libraries_path
+      redirect_to books_path
     else
         render 'new'
     end        
     end
+
+    def edit
+    @user = User.find(params[:id])
+    end
+
+    def update
+        @user=User.find(params[:id])
+        if @user.update(user_params)
+        flash[:success]= "Your account was successfully updated "
+        redirect_to books_path
+        else
+            render 'edit'
+        end 
+    end       
+
+
 
     private
     def user_params
